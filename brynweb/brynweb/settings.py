175dfe5643b2f333ab30fe2c0a9aab88bd1d9c38
Andrew Smith <andysmith.d@gmail.com>
2020-09-24 12:14:04 +0100

    Upgrade Django to 3.1.1

--- brynweb/brynweb/settings.py
+++ brynweb/brynweb/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+STATICFILES_STORAGE = 'pipeline.storage.PipelineManifestStorage'
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
