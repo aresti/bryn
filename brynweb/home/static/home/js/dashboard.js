6b0df3b319db68e9f405ac6f7f43cecf333207bf
Andrew Smith <andysmith.d@gmail.com>
2020-10-05 13:57:27 +0100

    Fixup jquery instance actions for bootstrap4 template changes

--- brynweb/home/static/home/js/dashboard.js
+++ brynweb/home/static/home/js/dashboard.js
@@ -21,11 +21,11 @@
     $.ajax({
       url: "/get_instances_table",
       type: "get",
-      data: {team_id: container.attr("data-team-id")},
+      data: { team_id: container.attr("data-team-id") },
 
-      success: function(json) {
+      success: function (json) {
         container.html(json.instances_table);
-      }
+      },
     });
   };
 
@@ -43,13 +43,11 @@
 
     event.preventDefault();
 
-    target.closest("ul").siblings(".dropdown-toggle").children("i:first-child").addClass("fa-spin fa-spinner");
-
     $.ajax({
       url: url,
       type: "get",
 
-      complete: function(xhr) {
+      complete: function (xhr) {
         var json = JSON.parse(xhr.responseText);
         var messages = teamContainer.find("div.messages-container");
         if ("messages_html" in json) {
@@ -57,8 +55,13 @@
           messages.html(json.messages_html);
         }
         messages.children().hide().fadeIn(500).delay(10000).fadeOut(500);
-        dashboard.refreshInstancesAfter(tableContainer, [1000, 2000, 5000, 10000]);
-      }
+        dashboard.refreshInstancesAfter(tableContainer, [
+          1000,
+          2000,
+          5000,
+          10000,
+        ]);
+      },
     });
   };
 
@@ -69,24 +72,32 @@
   */
 
   // AJAX forms
-  $(document).on('submit', 'form.launch-custom-form, form.launch-gvl-form', function(event){
-    event.preventDefault();
-    bryn.ajaxForms.formPOST(this, true, function (form) {
-      var tableContainer = $(form).closest('.team-container').find('.instances-table-container');
-      $('html, body').stop().animate({scrollTop: tableContainer.offset().top}, '500', 'swing');
-      dashboard.refreshInstancesAfter(tableContainer, [2000, 5000, 10000]);
-    });
-  });
+  $(document).on(
+    "submit",
+    "form.launch-custom-form, form.launch-gvl-form",
+    function (event) {
+      event.preventDefault();
+      bryn.ajaxForms.formPOST(this, true, function (form) {
+        var tableContainer = $(form)
+          .closest(".team-container")
+          .find(".instances-table-container");
+        $("html, body")
+          .stop()
+          .animate({ scrollTop: tableContainer.offset().top }, "500", "swing");
+        dashboard.refreshInstancesAfter(tableContainer, [2000, 5000, 10000]);
+      });
+    }
+  );
 
   // Instances refresh button
-  $(document).on("click", "button[data-team-action='refresh']", function(){
+  $(document).on("click", "button[data-team-action='refresh']", function () {
     var button = $(this);
     button.find("i").addClass("fa-spin fa-spinner");
     dashboard.refreshInstances(button.closest(".instances-table-container"));
   });
 
   // Instance actions
-  $(document).on("click", "ul.instance-actions-list > li > a", function (event) {
+  $(document).on("click", ".instance-actions-list > a", function (event) {
     event.preventDefault();
     var anchor = $(this);
     var url = anchor.attr("href");
@@ -98,22 +109,25 @@
     // Initially hide SSH key fields (as required)
     $("form.launch-custom-form").each(function () {
       var form = $(this);
-      if (form.find("select[name='server_key_name_choice']").val() != "bryn:new") {
+      if (
+        form.find("select[name='server_key_name_choice']").val() != "bryn:new"
+      ) {
         dashboard.hideSSHKeyFields(form);
       }
     });
 
     // SSH key fields how/hide on name change
-    $("form.launch-custom-form select[name='server_key_name_choice']").change(function () {
-      var keySelect = $(this);
-      var form = keySelect.closest("form.launch-custom-form");
+    $("form.launch-custom-form select[name='server_key_name_choice']").change(
+      function () {
+        var keySelect = $(this);
+        var form = keySelect.closest("form.launch-custom-form");
 
-      if (keySelect.val() == "bryn:new") {
-        dashboard.showSSHKeyFields(form);
-      } else {
-        dashboard.hideSSHKeyFields(form);
+        if (keySelect.val() == "bryn:new") {
+          dashboard.showSSHKeyFields(form);
+        } else {
+          dashboard.hideSSHKeyFields(form);
+        }
       }
-    });
+    );
   });
-  
-})(window.bryn = window.bryn || {}, jQuery);
+})((window.bryn = window.bryn || {}), jQuery);
