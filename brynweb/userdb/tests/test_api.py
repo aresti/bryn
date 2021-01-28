0fa79618e19fe041d317e506163bbeb38324a793
Andrew Smith <andysmith.d@gmail.com>
2021-01-28 10:29:37 +0000

    Additional tests for invitations api

--- brynweb/userdb/tests/test_api.py
+++ brynweb/userdb/tests/test_api.py
@@ -31,12 +31,14 @@ class TestInvitationAPI(APITestCase):
         pass
 
     def test_anon_user_cannot_get_invitation_list(self):
+        """Are non-authenticated users forbidden from list endpoint?"""
         response = self.client.get(
             reverse(self.path_name, kwargs={"team_id": self.team_a.pk})
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_admin_member_can_create_invitation(self):
+        """Can team admins create invitations?"""
         self.client.force_login(user=self.team_a_admin)
         data = {
             "to_team": self.team_a.hashid,
@@ -50,6 +52,7 @@ class TestInvitationAPI(APITestCase):
         self.assertEqual(Invitation.objects.count(), 1)
 
     def test_admin_member_can_view_invitations(self):
+        """Can team admins view invitations?"""
         InvitationFactory(to_team=self.team_a, made_by=self.team_a_admin)
         InvitationFactory(to_team=self.team_a, made_by=self.team_a_admin)
         self.client.force_login(user=self.team_a_admin)
@@ -60,6 +63,7 @@ class TestInvitationAPI(APITestCase):
         self.assertEqual(len(response.data), 2)
 
     def test_admin_from_another_team_cannot_view_invitations(self):
+        """Are team admins restricted to only seeing their own invitations?"""
         InvitationFactory(to_team=self.team_a, made_by=self.team_a_admin)
         self.client.force_login(user=self.team_b_admin)
         response = self.client.get(
@@ -68,6 +72,7 @@ class TestInvitationAPI(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_non_admin_cannot_view_invitations(self):
+        """Is a non-admin team member forbidden from viewing invitations?"""
         InvitationFactory(to_team=self.team_a, made_by=self.team_a_admin)
         self.client.force_login(user=self.team_a_member1)
         response = self.client.get(
@@ -75,7 +80,21 @@ class TestInvitationAPI(APITestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+    def test_invitiations_list_only_shows_correct_team(self):
+        """Does the invitations list only show teams for the team specified in the url?"""
+        InvitationFactory(to_team=self.team_a, made_by=self.team_a_admin)
+        InvitationFactory(to_team=self.team_a, made_by=self.team_a_admin)
+        InvitationFactory(to_team=self.team_b, made_by=self.team_b_admin)
+        InvitationFactory(to_team=self.team_b, made_by=self.team_b_admin)
+        self.client.force_login(user=self.team_a_admin)
+        response = self.client.get(
+            reverse(self.path_name, kwargs={"team_id": self.team_a.pk})
+        )
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0].get("to_team"), self.team_a.hashid)
+
     def test_non_admin_cannot_create_invitation(self):
+        """Are non-amdmin team members forbidden from creating invitations?"""
         self.client.force_login(user=self.team_a_member1)
         data = {
             "to_team": self.team_a.hashid,
@@ -88,6 +107,7 @@ class TestInvitationAPI(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_anon_user_cannot_get_invitation_detail(self):
+        """Are non-authorised users forbidden from viewing individual invitations?"""
         invitation = InvitationFactory(to_team=self.team_a, made_by=self.team_a_admin)
         response = self.client.get(
             reverse(
@@ -97,6 +117,7 @@ class TestInvitationAPI(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_admin_member_can_get_invitation_detail(self):
+        """Can team admins view individual invitations?"""
         invitation = InvitationFactory(to_team=self.team_a, made_by=self.team_a_admin)
         self.client.force_login(user=self.team_a_admin)
         response = self.client.get(
@@ -107,7 +128,8 @@ class TestInvitationAPI(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data.get("uuid"), str(invitation.uuid))
 
-    def test_non_admin_member_can_get_invitation_detail(self):
+    def test_non_admin_member_cannot_get_invitation_detail(self):
+        """Are non-admin team members forbidden from viewing individual invitations?"""
         invitation = InvitationFactory(to_team=self.team_a, made_by=self.team_a_admin)
         self.client.force_login(user=self.team_a_member1)
         response = self.client.get(
@@ -116,3 +138,15 @@ class TestInvitationAPI(APITestCase):
             )
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_admin_user_can_delete_an_invitation(self):
+        """Can a team admin delete an invitation?"""
+        invitation = InvitationFactory(to_team=self.team_a, made_by=self.team_a_admin)
+        self.client.force_login(user=self.team_a_admin)
+        response = self.client.delete(
+            reverse(
+                self.path_name, kwargs={"team_id": self.team_a.pk, "pk": invitation.pk},
+            )
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Invitation.objects.count(), 0)
