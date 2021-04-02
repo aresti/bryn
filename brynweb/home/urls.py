291790246e5bc4a174df84604f3347a1de56fcea
Andrew Smith <andysmith.d@gmail.com>
2021-04-02 11:13:18 +0100

    More explicit vue_urls; fixes admin url without trailing slash

--- brynweb/home/urls.py
+++ brynweb/home/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 app_name = "home"
 
-urlpatterns = [
+vue_urls = [
     path("", views.FrontendView.as_view(), name="home"),
-    path("<path:vue_router_path>", views.FrontendView.as_view(), name="home"),
+    path(
+        "teams/<path:vue_router_path>", views.FrontendView.as_view(), name="team_home"
+    ),
 ]
+
+urlpatterns = [path("", include(vue_urls))]
